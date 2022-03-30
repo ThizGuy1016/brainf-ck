32a2abee12dfea6c90cc5d5e085c13e631340c39
ThizGuy1016 <vavraacademy@gmail.com>
2022-03-30 16:37:52 -0500

    I hate commit messages

--- Python/bf.py
+++ Python/bf.py
@@ -1,161 +1,182 @@
 #!/usr/bin/env python3
-
+from enum import IntEnum, unique, auto
+from collections.abc import Sequence
 from pathlib import Path
-import argparse
 import sys
 
-iota_counter = 0
-def iota(reset: bool = False) -> int:
-    global iota_counter
-    if reset: iota_counter = 0
-    iota_counter += 1
-    return iota_counter
+STACK_MAX: int = 30000
 
-OP_INC=iota(True)   # 1 >
-OP_DEC=iota()       # 2 <
-OP_ADD=iota()       # 3 +
-OP_SUB=iota()       # 4 -
-OP_BLOOP=iota()     # 5 [
-OP_ELOOP=iota()     # 6 ]
-OP_INPUT=iota()     # 7 ,
-OP_OUTPUT=iota()    # 8 .
-OP_BSYSCALL=iota()  # 9 (
-OP_ESYSCALL=iota()  # 10 )
-COUNT_OPS=iota()    # 11
+@unique
+class OPCODE(IntEnum):
+    INC=auto()
+    DEC=auto()
+    ADD=auto()
+    SUB=auto()
+    BLOOP=auto()
+    ELOOP=auto()
+    INPUT=auto()
+    OUTPUT=auto()
+    BSYSCALL=auto()
+    ESYSCALL=auto()
+    NOOP=auto()
 
-def compile_program(program: list) -> None:
+OPCODE_PAIR: dict[chr, OPCODE] = {
+    '>': OPCODE.INC,
+    '<': OPCODE.DEC,
+    '+': OPCODE.ADD,
+    '-': OPCODE.SUB,
+    '[': OPCODE.BLOOP,
+    ']': OPCODE.ELOOP,
+    ',': OPCODE.INPUT,
+    '.': OPCODE.OUTPUT,
+    '(': OPCODE.BSYSCALL,
+    ')': OPCODE.ESYSCALL,
+}
+
+assert len(OPCODE_PAIR) == len(OPCODE) - 1, "Non exhaustive handiling of opcodes while parsing."
+
+OpAmount = int
+OpcodePair = tuple[OPCODE, OpAmount] # OP_Amount can also be a 
+Program = list[OpcodePair]
+Stack = list[int]
+StackCounter = list[int]
+
+def parse_program(path: Path) -> str:
+    with open(path, 'r') as p:
+        try: return p.read()
+        except Exception as e: raise Exception(f"File error: {e}")
+
+def lex_program(program_str: str) -> list[OPCODE]:
+    
+    """
+    Loops through symbols in program string and returns the opcode value of all valid symbols.
+    """
+    
+    opcode_list: list[OPCODE] = []
+    for symbol in program_str:
+        try: opcode_list.append(OPCODE_PAIR[symbol])
+        except: pass
+    return opcode_list
+
+def group_ops_recurs(opcode_list: list[OPCODE], program_buf: Program, program_counter: list[int], opcode_condition) -> None:
+    """
+    Allows grouping opcodes to be done recursively
+    """
+    
+    while True:
+    
+        if program_counter[0] >= len(opcode_list): return 
+        current_opcode = opcode_list[program_counter[0]]
+
+        if opcode_condition(current_opcode):
+            return
+
+        if current_opcode == OPCODE.BLOOP:
+            bloop_amount: Program = [(0, 0)]
+            program_counter[0] += 1
+            
+            group_ops_recurs(opcode_list, bloop_amount, program_counter, lambda x: x == OPCODE.ELOOP)
+            
+            try: del opcode_list[program_counter[0]] # removes the ending delimeter
+            except: assert False, "Unmatched closing delimeter for '['"
+            
+            #if len(bloop_amount[1:]) > 1: 
+            #    bloop_amount.append((OPCODE.NOOP, 1))
+            program_buf.append((OPCODE.BLOOP, tuple(bloop_amount[1:])))
+
+        # increase opcode amount if last opcode == current_opcode
+        elif program_buf[-1][0] == current_opcode:
+            program_buf[-1] = (program_buf[-1][0], program_buf[-1][1] + 1)
+            program_counter[0] += 1
+    
+        else:
+            program_buf.append((current_opcode, 1))
+            program_counter[0] += 1
+
+def group_ops(opcode_list: list[OPCODE]) -> Program:
+    
+    """
+    An optimization step to group identical opcodes into an opcode amount.
+    """
+
+    program_buf: Program = [(0, 0)]
+    program_counter: list[int] = [0]
+    for _ in enumerate(opcode_list):
+        group_ops_recurs(opcode_list, program_buf, program_counter, lambda x: False)
+
+    return program_buf[1:]
+
+def compile_program(program: Program) -> None:
     assert False, "Not Implemented"
 
-def group_loop(program_counter: int, program: list[int], ops: list[int, int]) -> None:
-    current_opcode: int = program[program_counter]
-    while_counter: int = program_counter
-    while current_opcode != OP_ELOOP:
-        assert while_counter < len(program) - 1, "Unmatched closing delimeter for '['" 
-        while_counter += 1
-        if program[while_counter] == OP_BLOOP:
-            group_loop(while_counter, program, ops)
-        current_opcode = program[while_counter]
-    del program[while_counter] # removes the ending loop opcode
-    del program[program_counter]
-    ops.append((OP_BLOOP, while_counter - 2)) # used to align the ending range of the loop with the starting range, change this is shit dont work
+def simulate_opcode(opcode: OpcodePair, stack_counter: StackCounter, stack: Stack) -> None:
+    """
+    Allows the simulation of opcodes to be done recursively
+    """
 
-def group_ops(program: list[int]) -> list[int]:
-    ops: list[int, int] = [(0, 0)] # (opcode, amount) / (opcode, closing delimeter)
-    program_counter: int = 0
-    for _ in enumerate(program):
-        if program_counter >= len(program):
-            continue
-        current_opcode: int = program[program_counter]
+    match opcode[0]:
+        case OPCODE.INC:
+            #print(f"Incrementing: {opcode[1]}")
+            stack_counter[0] += opcode[1]
+        case OPCODE.DEC:
+            #print(f"Decrementing: {opcode[1]}")
+            stack_counter[0] -= opcode[1]
         
-        # if we have a begining loop operation
-        if current_opcode == OP_BLOOP:
-            group_loop(program_counter, program, ops)
-
-        # the current instruction opcode is an endloop
-        elif current_opcode == OP_ELOOP: 
-            ops.append((OP_ELOOP, 1))
-
-        # if the last instruction opcode is the same as the current instruction opcode
-        elif ops[-1][0] == current_opcode:
-            ops[-1] = (ops[-1][0], ops[-1][1] + 1)
-
-        else:
-            ops.append((program[program_counter], 1))
-
-        program_counter += 1
-
-    return ops[1:]
-
-def simulate_program(program: list[int]) -> None:
-    STACK_MIN: int = 0
-    STACK_MAX: int = 1000
-
-    stack: list[int] = [0 for i in range(STACK_MIN,STACK_MAX)]    
-    stack_counter: int = 0
-    _ = simulate_opcodes(0, len(program) - 1, program, stack, stack_counter)
-
-def simulate_opcodes(program_counter: int, end_idx: int, program: list[int], stack: list[int], stack_counter: int) -> [int, int]:
-    while program_counter <= end_idx:
-
-        if stack_counter < 0: stack_counter = sys.maxsize
-        if stack[stack_counter] < 0: stack[stack_counter] = sys.maxsize
-
-        opcode: int = program[program_counter][0]
-        amount: int = program[program_counter][1]
-
-        if opcode == OP_INC:
-            stack_counter += amount
-
-        elif opcode == OP_DEC: 
-            stack_counter -= amount
-
-        elif opcode == OP_ADD: 
-            stack[stack_counter] += amount
+        case OPCODE.ADD:
+            #print(f"Adding: {opcode[1]}")
+            stack[stack_counter[0]] += opcode[1]
+        case OPCODE.SUB: 
+            #print(f"Subbing: {opcode[1]}")
+            stack[stack_counter[0]] -= opcode[1]
         
-        elif opcode == OP_SUB: 
-            stack[stack_counter] -= amount
+        case OPCODE.BLOOP:
 
-        elif opcode == OP_BLOOP:
             while True:
-                loop_begin: int = program_counter
-                # this is inefficient... but I refuse to write a class
-                assert amount <= end_idx, "Loops bounds exceded program size"
-                stack, stack_counter = simulate_opcodes(loop_begin + 1, amount, program, stack, stack_counter)
-                loop_begin = program_counter
-                if stack[stack_counter] == 0: 
-                    program_counter += 1
-                    break
+                #print(f"Looping")
+                for bloop_opcodes in opcode[1]:
+                    simulate_opcode(bloop_opcodes, stack_counter, stack)
+                if stack[stack_counter[0]] == 0: return
 
-        elif opcode == OP_OUTPUT: 
-            for _ in range(amount):
-                print((stack[stack_counter]))
-        elif opcode == OP_INPUT: 
-            for _ in range(amount):
+        case OPCODE.INPUT: 
+            for _ in range(opcode[1]):
                 x = input('')
                 assert x.isnumeric(), "Input was Nan"
-                stack[stack_counter] = int(x)
+                stack[stack_counter[0]] = int(x)
+        
+        case OPCODE.OUTPUT: 
+            for _ in range(opcode[1]):
+                print(chr(stack[stack_counter[0]]),end='')
+                pass
 
-        elif opcode == OP_BSYSCALL: assert False, "Not implemented"
-        elif opcode == OP_ESYSCALL: assert False, "Not implemented"
+        case OPCODE.BSYSCALL: assert False, "Not Implemented"
+        
+        case OPCODE.NOOP: 
+            for _ in range(opcode[1]):
+                continue
 
-        else: assert False, "Unknown opcode encountered during simulation"
+        case _ as e: 
+            assert False, f"Failed at instruction: {e}"
 
-        program_counter += 1
+    return
 
-    return stack, stack_counter
+def simulate_program(program: Program) -> None:
 
-def lex_program(contents: str) -> list[int]:
-    opcodes: dict = { '>': OP_INC, '<': OP_DEC, '+': OP_ADD, '-': OP_SUB, '[': OP_BLOOP, ']': OP_ELOOP, '(': OP_BSYSCALL, ')': OP_ESYSCALL }
-    return [opcodes[i] for i in contents if i in opcodes]
+    stack: Stack = [0 for i in range(STACK_MAX)]
+    stack_counter: StackCounter = [0]
 
-def read_program(path: Path) -> str:
-    with open(path, "r") as p:
-        try: return p.read()
-        except Exception as e: raise Exception(f"File error: Failed to read program file: {path}\n{e}")
+    for opcode in program:
+
+        if stack[stack_counter[0]] < 0: stack[stack_counter[0]] = sys.maxsize
+
+        simulate_opcode(opcode, stack_counter, stack)
 
 def main() -> None:
-    #program_path: Path = Path("hello.bf")
-    #contents = read_program(program_path)
-    #lexed_program = lex_program(contents)
-    #grouped = group_ops(lexed_program)
-    
-    grouped = group_ops([
-        OP_INC,
-        OP_ADD,
-        OP_INC,
-        OP_ADD,
-        OP_BLOOP,
-        OP_DEC,
-        OP_BLOOP,
-        OP_SUB,
-        OP_ELOOP,
-        OP_ELOOP,
-        ])
+    program_path = Path('hello.bf')
+    program = parse_program(program_path)
+    program = lex_program(program)
+    program = group_ops(program)
 
-    print(grouped)
-    print(len(grouped))
-
-    simulate_program(grouped)
+    simulate_program(program)
 
 if __name__ == "__main__":
     try: main()
